da7587a7d01a87e8d7c5825fa3da30b6a439953c
Zach <zach.meyer@outlook.com>
2026-08-24 12:30:01 -0500

    Removed examples, fixed workspace manifest errors.

--- examples/loading_spinners/README.md
+++
@@ -1,8 +0,0 @@
-## Loading Spinners
-
-Example implementation of animated indeterminate loading spinners.
-
-You can run it with `cargo run`:
-```
-cargo run --package loading_spinners
-```
--- examples/markdown/build.rs
+++
@@ -1,5 +0,0 @@
-pub fn main() {
-    // println!("cargo::rerun-if-changed=fonts/markdown-icons.toml");
-    // iced_fontello::build("fonts/markdown-icons.toml")
-    //     .expect("Build icons font");
-}
